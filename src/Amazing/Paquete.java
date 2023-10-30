171c2150ee4d04cc0174a8b1c11939fe819cac8c
ElioMolasDev <eliomolas.dev@gmail.com>
2023-10-30 19:48:41 -0300

    Subiendo Tp parte 2

---
+++ src/Amazing/Paquete.java
@@ -0,0 +1,77 @@
+package Amazing;
+
+public abstract class  Paquete {
+    private static int contadorPaquetes = 1; // Variable de clase para llevar un contador de pedidos
+    private static int idPaquete;
+    private int volumen;
+    private int  precio;
+    private static String direccionEntrega;
+    private static int costoDeEnvio;
+
+    public Paquete(int codPedido, int volumen, int precio, int porcentaje, int adicional) {
+        this.idPaquete = contadorPaquetes++;
+        this.volumen = volumen;
+        this.precio = precio;
+        this.direccionEntrega = obtenerDireccionPedido(codPedido);
+        this.costoDeEnvio = porcentaje * adicional;
+    }
+    public Paquete(int codPedido, int volumen, int precio, int porcentaje) {
+        this.idPaquete = contadorPaquetes++;
+        this.volumen = volumen;
+        this.precio = precio;
+        this.direccionEntrega = obtenerDireccionPedido(codPedido);
+        this.costoDeEnvio = porcentaje;
+    }
+
+
+    //CREA PAQUETE
+    public static Paquete crearPaquete(int codPedido, int volumen, int precio, int porcentaje, int adicional) {
+        if (codPedido < 0) {
+            throw new IllegalArgumentException("El ID del pedido no puede ser negativo.");
+        }
+
+        if (volumen <= 0) {
+            throw new IllegalArgumentException("El volumen del paquete debe ser mayor que cero.");
+        }
+
+        if (precio <= 0) {
+            throw new IllegalArgumentException("El precio del paquete debe ser mayor que cero.");
+        }
+
+        if (porcentaje <= 0) {
+            throw new IllegalArgumentException("El porcentaje/adicional del paquete debe ser mayor que cero.");
+        }
+
+        if (adicional < 0) {
+            throw new IllegalArgumentException("El adicional del paquete no puede ser negativo.");
+        }
+
+        return new Paquete(idPaquete, volumen, precio, porcentaje,  adicional);
+    }
+    public static Paquete crearPaquete(int codPedido, int volumen, int precio, int porcentaje) {
+        if (codPedido < 0) {
+            throw new IllegalArgumentException("El ID del pedido no puede ser negativo.");
+        }
+
+        if (volumen <= 0) {
+            throw new IllegalArgumentException("El volumen del paquete debe ser mayor que cero.");
+        }
+
+        if (precio <= 0) {
+            throw new IllegalArgumentException("El precio del paquete debe ser mayor que cero.");
+        }
+
+        if (porcentaje <= 0) {
+            throw new IllegalArgumentException("El porcentaje/adicional del paquete debe ser mayor que cero.");
+        }
+        return new Paquete(idPaquete, volumen, precio, porcentaje);
+    }
+
+    public String obtenerDireccionPedido(Integer codPedido) {
+        return "Pedido.obtenerDireccion(codPedido)";
+    }
+    public static Integer obtenerIdPaquete() {
+        return idPaquete;
+    }
+
+}
---
+++ src/Amazing/Pedido.java
@@ -0,0 +1,138 @@
+package Amazing;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Pedido {
+    private static int contadorPedidos = 1; // Variable de clase para llevar un contador de pedidos
+    private static int idPedido;
+    private int DNI;
+    private String nombreCliente;
+    private String direccionEntrega;
+    private HashMap<Integer, Paquete> carritoDePaquetes;
+    private Boolean pedidoCerrado;
+    private int precioDePedido;
+
+    public Pedido(String nombreCliente, String direccionEntrega, int DNI) {
+
+        if (nombreCliente == null || nombreCliente.isEmpty()) {
+            throw new IllegalArgumentException("El nombre del cliente no puede ser nulo o vacío.");
+        }
+
+        if (direccionEntrega == null || direccionEntrega.isEmpty()) {
+            throw new IllegalArgumentException("La dirección no puede ser nula o vacía.");
+        }
+
+        if (DNI <= 0) {
+            throw new IllegalArgumentException("El DNI debe ser un valor positivo.");
+        }
+
+        this.idPedido = contadorPedidos++;
+        this.DNI = DNI;
+        this.nombreCliente = nombreCliente;
+        this.direccionEntrega = direccionEntrega;
+        this.carritoDePaquetes = new HashMap<>();
+        this.pedidoCerrado = false;
+        this.precioDePedido = 0;
+    }
+    //CREA PEDIDO
+   // public static Pedido crearPedido(String nombreCliente, String direccionEntrega, int DNI) {
+
+    /*   if (nombreCliente == null || nombreCliente.isEmpty()) {
+            throw new IllegalArgumentException("El nombre del cliente no puede ser nulo o vacío.");
+      }
+
+        if (direccionEntrega == null || direccionEntrega.isEmpty()) {
+            throw new IllegalArgumentException("La dirección no puede ser nula o vacía.");
+        }
+
+        if (DNI <= 0) {
+            throw new IllegalArgumentException("El DNI debe ser un valor positivo.");
+        }
+
+        return new Pedido(nombreCliente, direccionEntrega, DNI);
+
+    }*/
+
+    // AGREGA PAQUETE A PEDIO
+    public static Integer agregarPaquete(int codPedido, int volumen, int precio, int porcentaje, int adicional) {
+    	Paquete paquete = 
+        carritoDePaquetes.put(idPaquete(), (Paquete.crearPaquete(codPedido, volumen,  precio,  porcentaje,  adicional)));
+        return idPaquete();
+    }
+
+    public Integer agregarPaquete(int codPedido, int volumen, int precio, int porcentaje) {
+        carritoDePaquetes.put(idPaquete(), (Paquete.crearPaquete(codPedido, volumen,  precio,  porcentaje)));
+        return idPaquete();
+    }
+
+    /*// QUITA PAQUETE D PEDIDO
+    public void quitarPaquete(int idPaquete) {
+        if (pedidoCerrado) {
+            throw new IllegalArgumentException("El pedido está cerrado, no se pueden quitar paquetes.");
+        }
+
+        Paquete paqueteAQuitar = null;
+        for (Paquete paquete : carritoDePaquetes) {
+            if (paquete.getId() == idPaquete) {
+                paqueteAQuitar = paquete;
+                break;
+            }
+        }
+
+        if (paqueteAQuitar != null) {
+            carritoDePaquetes.remove(paqueteAQuitar);
+            precioDePedido -= paqueteAQuitar.getCostoDeEnvio();
+        } else {
+            throw new IllegalArgumentException("El paquete con el ID especificado no se encuentra en el carrito.");
+        }
+    }
+
+    // CIERRA PEDIDO
+    public void cerrarPedido() {
+        if (pedidoCerrado) {
+            throw new IllegalArgumentException("El pedido ya está cerrado.");
+        }
+
+        pedidoCerrado = true;
+    }*/
+    //  DEVUELVE ID PEDIDO
+    public static Integer obtenerIdPedido() {
+        return idPedido;
+    }
+
+    //  DEVUELVE ID PAQUETE
+    private static Integer idPaquete() {
+        return Paquete.obtenerIdPaquete();
+    }
+
+    //  DEVUELVE PEDIDO A PARTIR DE ID
+    public static Pedido buscarPedido(int codPedido, HashMap<Integer, Pedido> listaPedidos) {
+        if (listaPedidos.containsKey(codPedido)) {
+            return listaPedidos.get(codPedido);
+        } else {
+            return null; // Devolver null si no se encuentra el pedido
+        }
+    }
+
+    // DEVUELVE PRECIO APAGAR DE CLEINTE
+    public int precioAPagar() {
+        return precioDePedido;
+    }
+
+    // DEVUELVE ESTADO DE PDIDO
+    public Boolean obtenerEstadoDePedido() {
+        return pedidoCerrado;
+    }
+
+    // DEVUELVE NOMBRE CLEINTE
+    public String obtenerCliente() {
+        return nombreCliente;
+    }
+
+    //DEVUELVE DIRECCION CLEINTE
+    public String obtenerDireccion() {
+        return this.direccionEntrega;
+    }
+
+}
